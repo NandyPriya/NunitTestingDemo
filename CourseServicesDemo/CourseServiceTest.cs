00d18fd19aa10099d556df94a1cfc2f95fd5a4b6
Nandhinipriya <nandhinipriya745@gmail.com>
2022-10-19 16:50:54 +0530

    Add project files.

---
+++ CourseServicesDemo/CourseServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using CourseUnitTestingDemo;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseServicesDemo
+{
+    [TestFixture]
+    public class CourseServiceTest
+    {
+        [TestCase(16, ExpectedResult = true)]
+        [TestCase(60, ExpectedResult = true)]
+        [TestCase(-1, ExpectedResult = false)]
+        [TestCase(-10, ExpectedResult = false)]
+        public bool GetAge(int fees)
+        {
+            StudentEnrollment employee = new StudentEnrollment();
+           Enrollment e = new Enrollment();
+            student s1 = new student();
+            e.Fees = fees;
+            bool ans = employee.Enrollstudent(s1,e);
+            // Assert.That(ans==true);
+            return ans;
+        }
+    }
+}
---
+++ CourseUnitTestingDemo/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseUnitTestingDemo
+{
+    public enum Coursename
+    {
+        AIML, Dotnet, Java
+    }
+    public class student
+        {
+        public int Rollno { get; set; }
+
+        public string Name { get; set; }
+
+        public string Coursename { get; set; }
+
+        public Coursename coursename { get; set; }
+    }
+    public class Enrollment
+    {
+        public int Fees { get; set; }
+        public int Enrollno { get; set; }
+
+    }
+    public class StudentEnrollment
+    {
+        public bool Enrollstudent(student s, Enrollment e)
+        {
+            if (e.Fees > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
